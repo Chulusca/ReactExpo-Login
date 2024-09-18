e1da063d4a9737355b3f182862e31df68752e0a8
tomas <czernuszkatomas@gmail.com>
2024-09-18 14:17:08 -0300

    Fetch Events

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,22 @@
-import React, {useContext} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import ButtonGradient from "../components/ButtonGradient";  
 import { AuthContext } from '../context/AuthContext';
 import { getEvents } from '../services/Events';
 
+
 export default function HomeScreen() {
   const { user, signOut } = useContext(AuthContext);
+  const [events, setEvents] = useState('');
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const fetchedEvents = await getEvents();
+      setEvents(fetchedEvents);
+    };
+
+    fetchEvents();
+  }, []);
 
   if (user.username == '') {
     return (
@@ -23,13 +34,26 @@ export default function HomeScreen() {
         <Text style={styles.email}>{user.username}</Text>
       </View>
       <ButtonGradient text={'Cargar Eventos'} funcion={getEvents} />
+      <View style={styles.scrollContainer}>
+        <ScrollView
+          style={styles.scrollView}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollViewContent}
+        >
+          {events.map((event) => (
+            <Text key={event.id} style={styles.text}>{event.name}</Text>
+          ))}
+        </ScrollView>
+      </View>
       <ButtonGradient text={'Sing Out'} funcion={signOut} />
     </View>
   );
 }
 
-
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({    
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -55,5 +79,17 @@ const styles = StyleSheet.create({
   email: {
     fontSize: 16,
     color: '#777',
+  },
+  scrollViewContent: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+  },
+  text: {
+    padding: 10,
+    fontSize: 18,
+  },
+  scrollContainer: {
+    height: 100, 
+    width: '100%',
   },
 });
